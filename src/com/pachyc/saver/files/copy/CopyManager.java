bd708fde17f9f00c84b12c34acc1c7aeeef07a86
AdamPach <adampach9@gmail.com>
2021-03-26 19:00:39 +0100

    Create pakage and class for copy

---
+++ src/com/pachyc/saver/files/copy/CopyManager.java
@@ -0,0 +1,11 @@
+package com.pachyc.saver.files.copy;
+
+import com.pachyc.saver.files.CopySave;
+
+public class CopyManager {
+    private CopySave copySave;
+
+    public CopyManager(CopySave copySave) {
+        this.copySave = copySave;
+    }
+}
