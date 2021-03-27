8a6d4627834188f4a94f03b0eddbcfd59b64a629
AdamPach <adampach9@gmail.com>
2021-03-27 13:37:15 +0100

    Function ver 1.0 build

--- src/com/pachyc/saver/files/copy/CopyManager.java
+++ src/com/pachyc/saver/files/copy/CopyManager.java
@@ -1,13 +1,16 @@
 package com.pachyc.saver.files.copy;
 
 import com.pachyc.saver.files.CopySave;
+import com.sun.nio.sctp.SctpStandardSocketOptions;
 
 import javax.swing.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.stream.Stream;
 
 public class CopyManager {
     private CopySave copySave;
@@ -20,28 +23,31 @@ public class CopyManager {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
         LocalDateTime localDateTime = LocalDateTime.now();
         String oldPath = this.copySave.getTo().toString();
-        this.copySave.setTo(new File(oldPath + File.separator + dtf.format(localDateTime)));
+        this.copySave.setTo(new File(oldPath + File.separator + dtf.format(localDateTime) + File.separator + this.copySave.getFrom().getName()));
         if(!this.copySave.getTo().mkdirs()){
             System.out.println("Nepovedlo se vytvorit slozku");
         }
         try{
-            Files.copy(this.copySave.getFrom().toPath(), this.copySave.getTo().toPath());
+            copyFolder(this.copySave.getFrom().toPath(), this.copySave.getTo().toPath());
         }
         catch (IOException e){
             System.err.println("Error: " + e.getMessage());
         }
     }
 
-    private void copyTree(Path source, Path target) throws IOException{
-        try (
-                InputStream in = new FileInputStream(source.toString());
-                OutputStream out = new FileOutputStream(target.toString())
-        ){
-            byte[] buf = new byte[1024];
-            int length;
-            while ((length = in.read(buf)) > 0){
-                out.write(buf, 0, length);
-            }
+    private void copyFolder(Path source, Path target) throws IOException{
+        try(Stream<Path> stream = Files.walk(source)){
+            stream.forEach(src -> copy(src, target.resolve(source.relativize(src))));
         }
     }
+
+    private void copy(Path source, Path target){
+        try{
+            Files.copy(source,target, StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
 }
